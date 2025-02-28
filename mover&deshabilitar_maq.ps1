bf86e7ed1357a2a01872282a316736b4d2cc09f2
Saith Barreto <33233515+saithbar@users.noreply.github.com>
2025-02-28 10:28:59 +0100

    Script para Mover & deshabilitar maquina del AD

---
+++ mover&deshabilitar_maq.ps1
@@ -0,0 +1,20 @@
+﻿# Ruta con las cuentas de equipo a deshabilitar
+$Computers = "C:\Users\ESGETDWINDOMADM02\Documents\Saith\Script\Maquinas_inactivas.txt"
+ 
+# Nombre de la OU donde se moverán las máquinas
+$OU_Destino = "ou=Deshabiitados,DC=ags-spain,DC=local"
+
+# Importar módulo de Powershell para Active Directory
+Import-Module ActiveDirectory
+
+# Carga lista de cuentas de equipo en una variable
+$ListarComputers = Get-Content $Computers
+
+# Deshabilita las cuentas de equipo
+$fecha = get-date -Format "d"
+$date = $fecha + "  fecha que fue Deshabilitado "
+$ListarComputers | Set-ADComputer -Description $date 
+$ListarComputers | Get-ADComputer | Disable-ADAccount 
+
+# Mueve las cuentas de equipo deshabilitadas a la OU de destino
+$ListarComputers | Get-ADComputer | Move-ADObject -TargetPath "ou=Estaciones,ou=Deshabiitados,DC=ags-spain,DC=local" 
