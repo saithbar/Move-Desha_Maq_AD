4830512cab24e9b3c19573627af1558b1a6934d5
Saith Barreto <33233515+saithbar@users.noreply.github.com>
2025-02-28 10:59:57 +0100

    Update mover&deshabilitar_maq.ps1

--- mover&deshabilitar_maq.ps1
+++ mover&deshabilitar_maq.ps1
@@ -1,15 +1,15 @@
  #*******Begin Comment**************
 .SYNOPSIS
 #	This script performs the  uninstallation of an application(s).
-#  Script for Ünistall MS TEAMS Clasic 
-# version MS TEAMS  1.6.0.1381
+#  Script for Move And Disable Maquine desde un TXT
+# version V01
 # Autor: Saith Barreto
 # Fecha: Septiembre 2024
 #*******End Comment**************# Ruta con las cuentas de equipo a deshabilitar
-$Computers = "C:\Users\ESGETDWINDOMADM02\Documents\Saith\Script\Maquinas_inactivas.txt"
+$Computers = "C:\Users\USER\Script\Maquinas_inactivas.txt"
  
 # Nombre de la OU donde se moverán las máquinas
-$OU_Destino = "ou=Deshabiitados,DC=ags-spain,DC=local"
+$OU_Destino = "ou=Deshabiitados,DC=nombreOU,DC=local"
 
 # Importar módulo de Powershell para Active Directory
 Import-Module ActiveDirectory
@@ -17,11 +17,11 @@ Import-Module ActiveDirectory
 # Carga lista de cuentas de equipo en una variable
 $ListarComputers = Get-Content $Computers
 
-# Deshabilita las cuentas de equipo
+# Deshabilita las cuentas de equipo (Comentario)
 $fecha = get-date -Format "d"
 $date = $fecha + "  fecha que fue Deshabilitado "
 $ListarComputers | Set-ADComputer -Description $date 
 $ListarComputers | Get-ADComputer | Disable-ADAccount 
 
 # Mueve las cuentas de equipo deshabilitadas a la OU de destino
-$ListarComputers | Get-ADComputer | Move-ADObject -TargetPath "ou=Estaciones,ou=Deshabiitados,DC=ags-spain,DC=local" 
+$ListarComputers | Get-ADComputer | Move-ADObject -TargetPath "ou=Estaciones,ou=Deshabiitados,DC=nombreOU,DC=local" 
